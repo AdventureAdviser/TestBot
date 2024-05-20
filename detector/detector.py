1dc87860a39c660327d0295216450b0611278faf
AdventureAdviser <adv8ntureadvis8r@gmail.com>
2024-05-20 19:58:22 +0300

    optimization

--- detector/detector.py
+++ detector/detector.py
@@ -4,20 +4,21 @@ import pygetwindow as gw
 from mss import mss
 from ultralytics import YOLO
 import asyncio
-import concurrent.futures
 import time
 
 # Load the YOLOv8 model on the GPU
+print("Загрузка модели YOLO...")
 model = YOLO('best.pt')
 model.to('cuda')
+print("Модель YOLO загружена")
 
 # Глобальные параметры для фреймрейта и масштаба
-current_fps = 30  # Изначальный фреймрейт
+current_fps = 120  # Зададим максимально возможный фреймрейт по умолчанию
 current_scale = 1.0  # Изначальный масштаб
 
-
-async def capture_window(window_title="ArkAscended"):
-    """ Захватывает видеопоток из указанного окна в реальном времени """
+async def capture_and_process_window(frame_queue, window_title="ArkAscended"):
+    """ Захватывает видеопоток из указанного окна, обрабатывает и отправляет в очередь """
+    print("Запуск захвата видеопотока...")
     sct = mss()
     window = gw.getWindowsWithTitle(window_title)[0]
     if window:
@@ -25,57 +26,50 @@ async def capture_window(window_title="ArkAscended"):
         monitor = {"top": window.top, "left": window.left, "width": window.width, "height": window.height}
         while True:
             try:
+                start_time = time.time()
                 screenshot = sct.grab(monitor)
                 frame = np.array(screenshot)
                 frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
-                yield frame
+
+                # Масштабируем изображение
+                if current_scale != 1.0:
+                    width = int(frame.shape[1] * current_scale)
+                    height = int(frame.shape[0] * current_scale)
+                    frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_LINEAR)
+
+                results = model(frame, device=0, workers=12)  # Обрабатываем кадр с использованием модели YOLOv8 на GPU
+                annotated_frame = results[0].plot()
+
+                await frame_queue.put(annotated_frame)
+
+                # Вычисляем время захвата и обработки кадра, и спим оставшееся время, чтобы поддерживать текущий фреймрейт
+                elapsed_time = time.time() - start_time
+                time_to_wait = max(0, (1.0 / current_fps) - elapsed_time)
+                await asyncio.sleep(time_to_wait)
             except Exception as e:
-                print(f"Произошла ошибка: {e}")
+                print(f"Произошла ошибка в захвате видеопотока: {e}")
                 break
 
-
-def process_frame(frame, scale, fps):
-    """ Обрабатывает захваченный кадр с использованием модели YOLOv8 для трекинга объектов """
-    start_time = time.time()
-
-    # Масштабируем изображение
-    if scale != 1.0:
-        width = int(frame.shape[1] * scale)
-        height = int(frame.shape[0] * scale)
-        frame = cv2.resize(frame, (width, height), interpolation=cv2.INTER_LINEAR)
-
-    results = model(frame)
-
-    # Используем встроенную функцию для отрисовки результатов
-    annotated_frame = results[0].plot()
-
-    # Контролируем фреймрейт
-    elapsed_time = time.time() - start_time
-    time_to_wait = max(0, (1.0 / fps) - elapsed_time)
-    time.sleep(time_to_wait)
-
-    return annotated_frame
-
-
-async def display_frame(frame):
-    """ Отображает обработанный кадр в окне """
-    cv2.imshow('Window Stream', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        cv2.destroyAllWindows()
-        return False
-    return True
-
+async def display_frame(frame_queue):
+    """ Отображает обработанные кадры из очереди """
+    while True:
+        frame = await frame_queue.get()
+        cv2.imshow('Window Stream', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
 
 async def main():
+    print("Запуск основного процесса...")
     window_title = "ArkAscended"
-    loop = asyncio.get_running_loop()
-    async for frame in capture_window(window_title):
-        with concurrent.futures.ThreadPoolExecutor() as pool:
-            processed_frame = await loop.run_in_executor(pool, process_frame, frame, current_scale, current_fps)
-            continue_display = await display_frame(processed_frame)
-            if not continue_display:
-                break
+    frame_queue = asyncio.Queue()
 
+    capture_task = asyncio.create_task(capture_and_process_window(frame_queue, window_title))
+    display_task = asyncio.create_task(display_frame(frame_queue))
+
+    await asyncio.gather(capture_task, display_task)
 
 if __name__ == "__main__":
+    print("Запуск программы...")
     asyncio.run(main())
+    print("Программа завершена")
--- main.py
+++ main.py
@@ -2,4 +2,6 @@ import asyncio
 from detector.detector import main as detector_main
 
 if __name__ == "__main__":
+    print("Запуск main.py")
     asyncio.run(detector_main())
+    print("main.py завершен")
